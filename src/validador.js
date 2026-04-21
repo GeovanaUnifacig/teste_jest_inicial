16dad2650c150180abbdb277fbd8c3a3b1455759
Geovana Gonçalves de Alvarenga Ramos <2510176@sempre.unifacig.edu.br>
2026-04-21 01:42:15 -0300

    Correção: adicionada verificação de tipo na função de email

--- src/validador.js
+++ src/validador.js
@@ -1,4 +1,8 @@
 function validarEmail(email) {
+  if (typeof email !== "string") {
+    throw new Error("O parâmetro precisa ser uma string");
+  }
+
   if (email.includes("@") && email.includes(".")) {
     return true;
   }
